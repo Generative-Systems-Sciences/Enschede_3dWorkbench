ad82a8cf5927c280f929da8b4e22704be546581c
Pirouz Nourian <genesis.lab.engineer@gmail.com>
2023-11-27 08:35:04 +0100

    added multiple functions to the brep voxelization code

--- Rhino8Python3/BrepVoxelizationGHPy3.py
+++ Rhino8Python3/BrepVoxelizationGHPy3.py
@@ -3,6 +3,7 @@ a = "Hello Python 3 in Grasshopper!"
 print(a)
 import Rhino.Geometry as rg
 import math
+import Grasshopper as gh
 print(type(solid))
 
 RBBox=solid.GetBoundingBox(rg.Plane.WorldXY)#https://developer.rhino3d.com/api/rhinocommon/rhino.geometry.geometrybase/getboundingbox
@@ -25,7 +26,7 @@ minVoxel.Y=minZ3.Y*v
 minVoxel.Z=minZ3.Z*w
 
 (m,n,o)=diagonalN3
-
+tol=10E-3
 print(m,n,o)
 print(type(m))
 voxelPoints=[]
@@ -38,10 +39,94 @@ for i in range(m+1):
             voxelPoints.append(voxelPoint)
             N3Voxels.append((i,j,k))
             cPoint=solid.ClosestPoint(voxelPoint)
-            distance=voxelPoint.DistanceTo(cPoint)
-            if not (solid.IsPointInside(voxelPoint,0.1,True)):
-                 distance=-distance
+            distance=cPoint.DistanceTo(voxelPoint)
+            if (solid.IsPointInside(voxelPoint, tol, True)):
+                distance=-distance
             distances.append(distance)
 a=voxelPoints
-b=N3Voxels# inspect these voxels and get comfortable with their abstract meaning, note the reason to 'embed' the voxel points in R3 in the line above
 d=distances
+
+def HeatColourANum(num):
+    grad=gh.GUI.Gradient.GH_Gradient.Heat()
+    grad.Linear=True
+    return grad.ColourAt(num)
+
+def boxelAVoxel(voxelPoint, voxelSize):
+    '''
+    v7_____________v6  
+    |\             |\  
+    | \            | \  
+    |  \ _____________\  
+    |   v4         |   v5  
+    |   |          |   |  
+    |   |          |   |  
+    v3--|----------v2  |  
+     \  |           \  |  
+      \ |            \ |  
+       \|             \|  
+        v0_____________v1
+    '''
+    u=voxelSize.X/2
+    v=voxelSize.Y/2
+    w=voxelSize.Z/2
+    v0=voxelPoint+rg.Vector3d(-u,-v,-w)
+    v1=voxelPoint+rg.Vector3d(+u,-v,-w)
+    v2=voxelPoint+rg.Vector3d(+u,+v,-w)
+    v3=voxelPoint+rg.Vector3d(-u,+v,-w)
+    v4=voxelPoint+rg.Vector3d(-u,-v,+w)
+    v5=voxelPoint+rg.Vector3d(+u,-v,+w)
+    v6=voxelPoint+rg.Vector3d(+u,+v,+w)
+    v7=voxelPoint+rg.Vector3d(-u,+v,+w)
+    vertices=[v0,v1,v2,v3,v4,v5,v6,v7]
+    box=rg.Mesh.CreateFromBox(vertices,1,1,1)
+    return box
+def cboxelAVoxel(voxelPoint, voxelSize, colour):
+    '''
+    v7_____________v6  
+    |\             |\  
+    | \            | \  
+    |  \ _____________\  
+    |   v4         |   v5  
+    |   |          |   |  
+    |   |          |   |  
+    v3--|----------v2  |  
+     \  |           \  |  
+      \ |            \ |  
+       \|             \|  
+        v0_____________v1
+    '''
+    u=voxelSize.X/2
+    v=voxelSize.Y/2
+    w=voxelSize.Z/2
+    v0=voxelPoint+rg.Vector3d(-u,-v,-w)
+    v1=voxelPoint+rg.Vector3d(+u,-v,-w)
+    v2=voxelPoint+rg.Vector3d(+u,+v,-w)
+    v3=voxelPoint+rg.Vector3d(-u,+v,-w)
+    v4=voxelPoint+rg.Vector3d(-u,-v,+w)
+    v5=voxelPoint+rg.Vector3d(+u,-v,+w)
+    v6=voxelPoint+rg.Vector3d(+u,+v,+w)
+    v7=voxelPoint+rg.Vector3d(-u,+v,+w)
+    vertices=[v0,v1,v2,v3,v4,v5,v6,v7]
+    box=rg.Mesh.CreateFromBox(vertices,1,1,1)
+    box.VertexColors.CreateMonotoneMesh(colour)
+    return box
+def remapNumbers(nums):
+    #remaps numbers into a range starting from the minimum and ending with the maximum of the list
+    remappedNs=[]
+    maxN=max(nums)
+    minN=min(nums)
+    for num in nums:
+        remappedN=(num-minN)/(maxN-minN)
+        remappedNs.append(remappedN)
+    return remappedNs
+remappedDists=remapNumbers(distances)
+colours=[]
+for remappedDist in remappedDists:
+    colour=HeatColourANum(remappedDist)
+    colours.append(colour)
+r=remappedDists#
+colouredBoxes=[]
+for i in range(len(distances)):
+    colouredBox=cboxelAVoxel(voxelPoints[i],size,HeatColourANum(remappedDists[i]))
+    colouredBoxes.append(colouredBox)
+c=colouredBoxes
